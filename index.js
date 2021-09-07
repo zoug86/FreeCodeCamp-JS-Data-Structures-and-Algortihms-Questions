c889343d3482e8de183c1f07b758194774f0864b
Houssem Marzougui <marzouguih86@gmail.com>
2021-09-06 18:53:00 -0700

    added recursion

--- index.js
+++ index.js
@@ -87,3 +87,96 @@
 // updateRecords(recordCollection, 2548, "tracks", "");
 
 // console.log(recordCollection)
+
+// ----------------------------------------------------------------------------------------------------
+
+// Setup
+// var contacts = [
+//     {
+//         "firstName": "Akira",
+//         "lastName": "Laine",
+//         "number": "0543236543",
+//         "likes": ["Pizza", "Coding", "Brownie Points"]
+//     },
+//     {
+//         "firstName": "Harry",
+//         "lastName": "Potter",
+//         "number": "0994372684",
+//         "likes": ["Hogwarts", "Magic", "Hagrid"]
+//     },
+//     {
+//         "firstName": "Sherlock",
+//         "lastName": "Holmes",
+//         "number": "0487345643",
+//         "likes": ["Intriguing Cases", "Violin"]
+//     },
+//     {
+//         "firstName": "Kristian",
+//         "lastName": "Vos",
+//         "number": "unknown",
+//         "likes": ["JavaScript", "Gaming", "Foxes"]
+//     }
+// ];
+
+
+// function lookUpProfile(name, prop) {
+//     // Only change code below this line
+//     // if the both exist
+//     var count1 = 0;
+//     var count2 = 0;
+//     for (var i = 0; i < contacts.length; i++) {
+//         if (contacts[i]['firstName'] === name && contacts[i].hasOwnProperty(prop)) {
+//             return contacts[i][prop];
+//         }
+//         if (contacts[i]['firstName'] !== name) {
+//             count1++;
+//             if (count1 === contacts.length) {
+//                 return 'No such contact';
+//             }
+//         }
+
+//         if (!contacts[i].hasOwnProperty(prop)) {
+//             count2++;
+//             if (count2 === contacts.length) {
+//                 return 'No such property';
+//             }
+//         }
+
+//     }
+//     console.log(count1, count2);
+
+// }
+
+
+// console.log(lookUpProfile("Akira", "likes"));
+// console.log(lookUpProfile("Kristian", "lastName"));
+// console.log(lookUpProfile("Sherlock", "likes"));
+// console.log(lookUpProfile("Harry", "likes"));
+// console.log(lookUpProfile("Akira", "address"));
+// console.log(lookUpProfile("Bob", "potato"));
+// console.log(lookUpProfile("Bob", "number"));
+
+// ----------------------------------------------------------------------------------------------
+// var arr = []
+// function countdown(n) {
+//     if (n < 1) {
+//         return [];
+//     }
+
+//     arr.push(n);
+//     countdown(n - 1);
+//     return arr;
+// }
+
+// console.log(countdown(20));
+
+// ----------------------------------------------------------------------------------------------
+var arr = [];
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum > endNum) return [];
+    arr.push(startNum);
+    rangeOfNumbers(startNum + 1, endNum);
+    return arr;
+}
+
+console.log(rangeOfNumbers(10, 20));
